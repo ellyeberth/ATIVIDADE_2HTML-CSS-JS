5a063c3bd9e8a54ec519ca5895c95261785768b8
ellyeberth <80013248+ellyeberth@users.noreply.github.com>
2021-05-02 15:54:49 -0300

    Atualização de Questão 2
    Juros em parcelamento em 3x, 6x e alinhamento de botão "Ver Preços".

--- Atividade-2_HTML-CSS-JS/js/script-q2.js
+++ Atividade-2_HTML-CSS-JS/js/script-q2.js
@@ -10,11 +10,13 @@ var parcelas = document.querySelector('#parcelas');
 var valortotal = document.querySelector('#valortotal');
 var carne = document.querySelector('#carne');
 var valoriphone = parseFloat();
+var juros = parseFloat();
 
 
 botaopreco.addEventListener('click', () => {
 
-    if (produto.value != "" && qtde.value != "") {
+    if (produto.value != "Selecione o Produto" && qtde.value != "" && parcelas.value != "Parcelas") {
+
 
         if (produto.value == "iPhone XR") {
             valoriphone = 4999;
@@ -28,17 +30,47 @@ botaopreco.addEventListener('click', () => {
             valoriphone = 7999;
         } else if (produto.value == "iPhone 12 Pro") {
             valoriphone = 10999;
+        } else if (produto.value == "MacBook Air") {
+            valoriphone = 12999;
+        } else if (produto.value == "Macbook Pro 13 pol.") {
+            valoriphone = 17299;
+        } else if (produto.value == "Macbook Pro 16 pol.") {
+            valoriphone = 30299;
+        } else if (produto.value == "iMac 27 pol.") {
+            valoriphone = 30749;
+        } else if (produto.value == "Mac Pro (Server)") {
+            valoriphone = 79499;
         } else {
             valoriphone = 0;
         }
 
-        valorproduto.innerHTML = "R$" + parseFloat(valoriphone) .toFixed(2);
+        switch (parcelas.value) {
+            case "1":
+                juros = 0;
+            break;
+            case "2":
+                juros = 0;
+            break;
+            case "3":
+                juros = 0.03;
+            break;
+            case "4":
+                juros = 0;
+            break;
+            case "5":
+                juros = 0;
+            break;
+            case "6":
+                juros = 0.12;
+            break;
+        }
 
-        valortotal.innerHTML = "R$" + parseFloat(valoriphone * qtde.value) .toFixed(2);
-
+        valorproduto.innerHTML = "R$" + parseFloat(valoriphone).toFixed(2);
+        
+        valortotal.innerHTML = "R$" + parseFloat((valoriphone * qtde.value) + valoriphone * qtde.value * juros).toFixed(2);
 
     } else {
-        alert('Erro ao calcular o Valor Total. Campo Quantidade está vazio!')
+        alert('Existe campos vazios! Preencha-os...');
     }
 })
 
@@ -47,11 +79,12 @@ botaogerar.addEventListener('click', () => {
 
     if (cliente.value != "" && produto.value != "Selecione o Produto" && qtde.value != "" && parcelas.value != "Parcelas") {
 
+
         for (var cont = 1; cont <= parcelas.value; cont++) {
 
             carne.innerHTML = carne.innerHTML + "Cliente: " + cliente.value + "<br>" +
                 "Produto: " + produto.value + "<br>" +
-                "Valor: <b>R$" + ((valoriphone * qtde.value) / parcelas.value).toFixed(2) + "<b><br><br>" +
+                "Valor: <b>R$" + (((valoriphone * qtde.value) + valoriphone * qtde.value * juros) / parcelas.value).toFixed(2) + "<b><br><br>" +
                 "Parcelas: <b>" + cont + "/" + parcelas.value + "<b><br><br><hr><br><br>";
         }
 
